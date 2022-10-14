891a39c65381ec0d34a7f400a636077bd7b85258
sanjeevsaffron99 <sanjeev.saffron1999@gmail.comgit config --global user.name sanjeevsaffron99git config --global user.email sanjeev.saffron1999@gmail.com>
2022-10-14 21:49:48 +0530

    add logger service

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express' 
+import { Request,Response } from 'express';
 import 'reflect-metadata'                 
 import { createConnection } from 'typeorm'     
 var bodyParser = require('body-parser')     
@@ -8,7 +8,51 @@ const helmet = require("helmet");
 import routes from "./routes/routes";       
 var morgan = require('morgan')         
 require("dotenv").config();     
-                            
+import * as winston from "winston";
+//import * as morgan from "morgan";
+
+export const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      level: 'debug',
+      handleExceptions: true,
+      format: winston.format.combine(
+        winston.format.timestamp({ format: 'HH:mm:ss:ms' }),
+        winston.format.colorize(),
+        winston.format.printf(
+          (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+        ),
+        //  winston.format.simple(),
+      ),
+    }),
+  ],
+  exitOnError: false,
+});
+
+if (process.env.NODE_ENV === "dev") {
+  logger.add(
+    new winston.transports.File({
+      level: 'info',
+      filename: './logs/all-logs.log',
+      handleExceptions: true,
+      format: winston.format.combine(
+        winston.format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        winston.format.errors({ stack: true }),
+        winston.format.printf(
+          (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+        ),
+        // winston.format.splat(),
+        // winston.format.json()
+      ),
+      maxsize: 5242880, //5MB
+      maxFiles: 5,
+    }));
+}
+logger.info("logging started");
+
+                     
 createConnection()            
   .then(async () => {                            
     // Create a new express application instance 
@@ -19,9 +63,20 @@ createConnection()
     app.use(helmet());                                    
     app.use(bodyParser.urlencoded({extended:true}));  
     app.use(bodyParser.json()); 
-    app.use(morgan('dev'));  
-                                             
-    //Set all routes from routes folder 
+   // app.use(morgan('dev'));  
+    app.use(morgan(function (tokens:any,req:Request, res:Response) {
+      const msg = [
+          tokens.method(req, res),
+          tokens.url(req, res),
+          tokens.status(req, res),
+          tokens.res(req, res, 'content-length'), '-',
+          tokens['response-time'](req, res), 'ms',
+      ].join(' ');
+      logger.http(msg);
+      return null;
+      // return msg;
+  })
+  );          //Set all routes from routes folder 
     app.use("/", routes);  
                                  
     app.listen(8000, () => {                        
---
+++ src/services/logger.ts
@@ -0,0 +1,24 @@
+const winston = require('winston');
+
+let logger = new winston.Logger({
+  transports: [
+    new winston.transports.File({
+      level: 'verbose',
+      timestamp: new Date(),
+      filename: 'filelog-verbose.log',
+      json: false,
+    }),
+    new winston.transports.File({
+      level: 'error',
+      timestamp: new Date(),
+      filename: 'filelog-error.log',
+      json: false,
+    })
+  ]
+});
+
+logger.stream = {
+  write: function(message, encoding) {
+    logger.info(message);
+  }
+};
